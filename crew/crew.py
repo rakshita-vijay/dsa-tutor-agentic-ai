f7404f08f92e6a9a8e4008f4998f37e0c9ea4436
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-06-22 20:37:57 +0530

    Update crew.py

--- crew/crew.py
+++ crew/crew.py
@@ -1,1 +1,17 @@
+from crewai import Crew
+from llm.gemini import my_llm
 
+from agents import concept_explainer, problem_generator, evaluator, debugger, doubt_solver, feedback_collector, progress_tracker, resource_recommender 
+
+from tasks import concept_explanation, problem_generation, solution_evaluation, code_debugging, doubt_resolution, feedback_collection, progress_tracking, resource_recommendation
+
+crewww = Crew(
+  agents = [concept_explainer, problem_generator, evaluator, debugger, doubt_solver, feedback_collector, progress_tracker, resource_recommender],
+  tasks = [concept_explanation, problem_generation, solution_evaluation, code_debugging, doubt_resolution, feedback_collection, progress_tracking, resource_recommendation],
+  process = "sequential",
+  verbose = False,
+  memory = False,
+  share_crew = True,
+  planning = False,
+  chat_llm = my_llm
+)
