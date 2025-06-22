2cdad88b27f1ac1594021de237e9545b5fa5ff1f
anushkadoshii <148350571+anushkadoshii@users.noreply.github.com>
2025-06-22 17:50:25 +0530

    Update gemini.py

--- llm/gemini.py
+++ llm/gemini.py
@@ -1,1 +1,10 @@
+from crewai import LLM
 
+api_key = "AIzaSyDFO8_NWA5nV08fpZVOjZrdSUN8StjGnbk"
+
+my_llm = LLM(
+  model = 'gemini/gemini-2.5-flash',
+  api_key = api_key,
+  temperature = 0.6,
+  max_tokens = 1024
+)
