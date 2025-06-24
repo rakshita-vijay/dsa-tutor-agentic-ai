0fe9993e375510a2a56bdfa89a4b6dfb643dafa9
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-06-24 17:50:26 +0530

    Update crew.py

--- crew/crew.py
+++ crew/crew.py
@@ -1,8 +1,8 @@
 from crewai import Crew
 from llm.gemini import my_llm
 
-from agents import concept_explainer, problem_generator, evaluator, debugger, doubt_solver, feedback_collector, progress_tracker, resource_recommender 
-
+from agents import concept_explainer, mcq_question_asker, coding_question_asker, answer_checker, debugger, doubt_solver, feedback_provider, progress_tracker, resource_recommender 
+ 
 from tasks import concept_explanation, problem_generation, solution_evaluation, code_debugging, doubt_resolution, feedback_collection, progress_tracking, resource_recommendation
 
 crewww = Crew(
