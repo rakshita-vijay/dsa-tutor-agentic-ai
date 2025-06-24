a4f32b35373dae68f4247fb0ac20e5622834c054
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-06-24 18:33:38 +0530

    Update gemini.py

--- llm/gemini.py
+++ llm/gemini.py
@@ -1,20 +1,20 @@
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
-# from crewai import LLM
+from crewai import LLM
 
 load_dotenv()
 
 def get_gemini_model():
-    api_key = os.getenv("GEMINI_API_KEY")
-    if not api_key:
-        raise ValueError("GEMINI_API_KEY not found in .env")
-    genai.configure(api_key=api_key)
-    return genai.GenerativeModel("gemini-2.0-flash")
+  api_key = os.getenv("GEMINI_API_KEY")
+  if not api_key:
+      raise ValueError("GEMINI_API_KEY not found in .env")
+  genai.configure(api_key=api_key)
+  return genai.GenerativeModel("gemini-2.0-flash")
 
-# my_llm = LLM(
-#   model = 'gemini/gemini-2.5-flash',
-#   api_key = api_key,
-#   temperature = 0.6,
-#   max_tokens = 1024
-# )
+my_llm = LLM(
+  model = 'gemini/gemini-2.0-flash',
+  api_key = os.getenv("GEMINI_API_KEY"),
+  temperature = 0.8,
+  max_tokens = 2048
+)
